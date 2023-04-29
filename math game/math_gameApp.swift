86f5138e25559decc8f509cbbd05a4b19a1f25bd
Michael Spain <m.spain@setonhill.edu>
2023-04-29 14:42:48 -0400

    Initial Commit

---
+++ math game/math_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  math_gameApp.swift
+//  math game
+//
+//  Created by Michael spain on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct math_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
